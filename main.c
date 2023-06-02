b96594e6013911c74735d29c46157cc6579a36d2
JThanasi22 <128150301+JThanasi22@users.noreply.github.com>
2023-06-02 15:50:54 +0200

    Adding functions

--- main.c
+++ main.c
@@ -1,14 +1,41 @@
-//
-//  main.c
-//  sudok
-//
-//  Created by Jord Thanasi on 29.5.23.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "struct.h"
+void printEnteredValues(int arr1[][9], int i, int j)
+{
+    for( i=0;i<9;i++)
+    {
+        printf("|");
+        for( j=0;j<9;j++)
+        {
+            printf(" %d ", arr1[i][j]);
+            if (((j + 1) % 3) == 0)
+            {
+                printf("|");
+            }
+        }
+        printf("\n");
+        if (((i + 1) % 3) == 0)
+        {
+            printf("-------------------------------\n");
+        }
+    }
+}
+void getData(int arr[][9], int arr1[][9], int i, int j)
+{
+    for( i=0;i<9;i++)
+    {
+        printf("Enter row %d:\n", i+1);
+        for( j=0;j<9;j++)
+        {
+            scanf("%d", &arr[i][j]);
+            arr1[i][j]=arr[i][j];
+            if(arr[i][j] == 0)
+                arr[i][j] = -1;
+        }
+    }
+}
 int solve(int arr[][9], int x, int y)
 {
 
@@ -45,36 +72,10 @@ int main()
     printf("Welcome to Sudoku solver \n------------------------\n");
     printf("IMPORTANT: \n----------\n");
     printf("1) Enter every row separately \n2) Range of accepted numbers are from 1 to 9 \n3) For empty places put 0 \n4) If the layout is unsolvable, the program will print:UNSOLVABLE\n");
-    for( i=0;i<9;i++)
-    {
-        printf("Enter row %d:\n", i+1);
-        for( j=0;j<9;j++)
-        {
-            scanf("%d", &arr[i][j]);
-            arr1[i][j]=arr[i][j];
-            if(arr[i][j] == 0)
-                arr[i][j] = -1;
-        }
-    }
+    getData(arr, arr1, i, j);
     printf("The entered values are:\n");
     printf("-------------------------------\n");
-    for( i=0;i<9;i++)
-    {
-        printf("|");
-        for( j=0;j<9;j++)
-        {
-            printf(" %d ", arr1[i][j]);
-            if (((j + 1) % 3) == 0)
-            {
-                printf("|");
-            }
-        }
-        printf("\n");
-        if (((i + 1) % 3) == 0)
-        {
-            printf("-------------------------------\n");
-        }
-    }
+    printEnteredValues(arr1, i, j);
     int res = solve(arr,0,0);
     printf("The given puzzle is %s\n\n", (res==1?"SOLVABLE":"UNSOLVABLE"));
     
